ec507b4d3ba69436242b08b53e11dbece2e7823e
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2023-11-29 19:59:46 +0800

    别名注册表 SimpleAliasRegistry 处理顺序的相关注释

--- spring-context/src/main/java/org/springframework/Main.java
+++ spring-context/src/main/java/org/springframework/Main.java
@@ -1,9 +1,11 @@
 package org.springframework;
 
+import org.springframework.beans.factory.support.AbstractBeanFactory;
 import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.SimpleAliasRegistry;
 
 import java.util.stream.Stream;
 
@@ -19,7 +21,13 @@ public class Main {
 
 	@Configuration
 	static class TestConfiguration {
-
+		/**
+		 * 别名 默认传入的第一个是name 后面的都是别名 放在别名注册表({@link SimpleAliasRegistry#aliasMap})中
+		 * {@link AbstractBeanFactory#doGetBean(String, Class, Object[], boolean)}
+		 * {@link AbstractBeanFactory#transformedBeanName(String)}
+		 * {@link SimpleAliasRegistry#canonicalName(String)}
+		 * {@link SimpleAliasRegistry#aliasMap}
+		 */
 		@Bean({"u1","u2"})
 		public User UserOne() {
 			return new User("张小三");
