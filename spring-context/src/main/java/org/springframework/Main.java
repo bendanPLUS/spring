53e8d6ccd3b068fd18e7cd530e8c8f2a54aed242
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2023-11-30 14:19:37 +0800

    为了别名的解析测试

--- spring-context/src/main/java/org/springframework/Main.java
+++ spring-context/src/main/java/org/springframework/Main.java
@@ -14,13 +14,14 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-		new AnnotatedBeanDefinitionReader(ctx).register(TestConfiguration.class);
+		new AnnotatedBeanDefinitionReader(ctx).register(TestAlisaTransformedBeanName.class);
 		ctx.refresh();
 		Stream.of(ctx.getBeanFactory().getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
+	//为了别名的解析测试
 	@Configuration
-	static class TestConfiguration {
+	static class TestAlisaTransformedBeanName {
 		/**
 		 * 别名 默认传入的第一个是name 后面的都是别名 放在别名注册表({@link SimpleAliasRegistry#aliasMap})中
 		 * {@link AbstractBeanFactory#doGetBean(String, Class, Object[], boolean)}
@@ -28,12 +29,12 @@ public class Main {
 		 * {@link SimpleAliasRegistry#canonicalName(String)}
 		 * {@link SimpleAliasRegistry#aliasMap}
 		 */
-		@Bean({"u1","u2"})
+		@Bean({"u1","u2","u3"})
 		public User UserOne() {
 			return new User("张小三");
 		}
 
-		@Bean({"u5","u6"})
+		@Bean({"u5","u6","u7"})
 		public User UserTwo() {
 			return new User("张大三");
 		}
