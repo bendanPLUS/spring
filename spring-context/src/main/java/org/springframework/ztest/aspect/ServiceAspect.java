fcafb67227c0a0375ade6f6e6256be0c08f8bc13
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2023-12-02 17:32:07 +0800

    深入理解advisor增强器的概念: 通知方法+注解上的切入点方法=增强器 Advisor = pointcutMethod+method

--- spring-context/src/main/java/org/springframework/ztest/aspect/ServiceAspect.java
+++ spring-context/src/main/java/org/springframework/ztest/aspect/ServiceAspect.java
@@ -1,10 +1,7 @@
 package org.springframework.ztest.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,21 +9,20 @@ import org.springframework.stereotype.Component;
 public class ServiceAspect {
 
 	@Pointcut("execution(public * org.springframework.ztest.aspect.service.*.*(..))")
-	public void pointcut() {
+	public void pointcut切入点方法() {
 	}
 
-
-	@Before("pointcut()")
-	public void beforePrint(JoinPoint joinPoint) {
+	@Before("pointcut切入点方法") // 一个通知方法+注解上的切入点方法相当于一个增强器(Advisor=一个切入点方法+一个通知方法)
+	public void beforePrint通知方法(JoinPoint joinPoint) {
 		System.out.println("Service Aspect before advice run ......");
 	}
 
-	@After("pointcut()")
-	public void afterPrint() {
+	@After("pointcut切入点方法")  //一个通知方法+注解上的切入点方法相当于一个增强器
+	public void afterPrint通知方法() {
 		System.out.println("Service Aspect After advice run ......");
 	}
 
-	//@Around("pointcut()")
+	//@Around("pointcut()") //一个通知方法+注解上的切入点方法相当于一个增强器
 	public void aroundPrint(JoinPoint joinPoint) {
 		System.out.println("Service Aspect Around advice run ......");
 	}
