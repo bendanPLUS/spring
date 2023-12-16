4c197d9780a71a7440a24cb336be84328d250bab
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2023-12-16 19:22:35 +0800

    注解@EnableAspectJAutoProxy是AOP开关

--- spring-context/src/main/java/org/springframework/ztest/aspect/configuration/AspectJAutoProxyConfig.java
+++ spring-context/src/main/java/org/springframework/ztest/aspect/configuration/AspectJAutoProxyConfig.java
@@ -3,7 +3,8 @@ package org.springframework.ztest.aspect.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy //AOP 开关
+// 注解@EnableAspectJAutoProxy是AOP开关
+@EnableAspectJAutoProxy
 @Configuration
 public class AspectJAutoProxyConfig {
 }
