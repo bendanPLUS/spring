1fb3d6e78c943c8812671593122791ee4f32d647
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2023-12-17 18:46:30 +0800

    bean实例化前的后置处理器 对于实现Aware相关接口的类,进行Aware处理 重写Set方法

--- spring-context/src/main/java/org/springframework/ztest/aware/ForTestAware.java
+++ spring-context/src/main/java/org/springframework/ztest/aware/ForTestAware.java
@@ -10,9 +10,15 @@ public class ForTestAware implements ApplicationContextAware {
 
 	private ApplicationContext applicationContext;
 
+	/**
+	 *  bean实例化前的后置处理器 对于实现Aware相关接口的类,进行Aware处理 重写Set方法
+	 *  set时机:
+	 * 	{@link org.springframework.context.support.ApplicationContextAwareProcessor#postProcessBeforeInitialization(Object, String)}
+	 */
+
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		System.out.println("调用实例化前的后置处理器 对于实现Aware相关接口的类,进行Aware Set ApplicationContextAware");
+		System.out.println("bean实例化前的后置处理器 对于实现Aware相关接口的类,进行Aware Set ApplicationContextAware");
 		this.applicationContext = applicationContext;
 	}
 }
