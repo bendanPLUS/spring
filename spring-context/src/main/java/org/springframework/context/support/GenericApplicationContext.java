33dc14aa3765ca6bf2f5affeb10f8b4821d7bd0a
duyu <duyuvipkobe24@163.com>
2024-11-18 11:32:20 +0800

    public GenericApplicationContext() { this.beanFactory = new DefaultListableBeanFactory();}

--- spring-context/src/main/java/org/springframework/context/support/GenericApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/support/GenericApplicationContext.java
@@ -294,7 +294,7 @@ public class GenericApplicationContext extends AbstractApplicationContext implem
 			throw new IllegalStateException(
 					"GenericApplicationContext does not support multiple refresh attempts: just call 'refresh' once");
 		}
-		this.beanFactory.setSerializationId(getId());
+		this.beanFactory.setSerializationId(getId()); // this.beanFactory = new DefaultListableBeanFactory();
 	}
 
 	@Override
