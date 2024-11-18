f1841c9bb690e5203b0bbdae86066e139bab87af
duyu <duyuvipkobe24@163.com>
2024-11-18 14:21:15 +0800

    对ApplicationListener类型对 进行额外对处理

--- spring-context/src/main/java/org/springframework/context/support/ApplicationListenerDetector.java
+++ spring-context/src/main/java/org/springframework/context/support/ApplicationListenerDetector.java
@@ -71,7 +71,7 @@ class ApplicationListenerDetector implements DestructionAwareBeanPostProcessor, 
 
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String beanName) {
-		if (bean instanceof ApplicationListener<?> applicationListener) {
+		if (bean instanceof ApplicationListener<?> applicationListener) { // 对ApplicationListener类型对 进行额外对处理
 			// potentially not detected as a listener by getBeanNamesForType retrieval
 			Boolean flag = this.singletonNames.get(beanName);
 			if (Boolean.TRUE.equals(flag)) {
